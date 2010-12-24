3471a34a13b939e88781eec2e21a0d505a22530e
Kurt Stephens <ks.github@kurtstephens.com>
2010-12-24 03:42:39 -0600

    cleanup.

--- src/symbol.c
+++ src/symbol.c
@@ -1,25 +1,24 @@
+/* -*- c -*- */
 #include "tort/core.h"
 
-
 /********************************************************************/
-
 
 tort_v tort_symbol_make(const char *string)
 {
   if ( string ) {
-  tort_map_entry *e = _tort_m_map__get_entry_cstr(0, _tort->symbols, string);
-  if ( e ) {
-    // fprintf(stderr, "\n old symbol = %s %p\n", tort_symbol_data(e->value), (void *) e->value);
-    return e->value;
-  } else {
-    tort_v key, value;
-    key = tort_string_new_cstr(string);
-    value = tort_allocate(0, 0, sizeof(tort_symbol), _tort->_mt_symbol);
-    tort_ref(tort_symbol, value)->name = key;
-    _tort_m_map__add(0, _tort->symbols, key, value);
-    // fprintf(stderr, "\n new symbol = %s %p\n", tort_symbol_data(value), (void *) value);
-    return value;
-  } 
+    tort_map_entry *e = _tort_m_map__get_entry_cstr(tort_thread_arg _tort->symbols, string);
+    if ( e ) {
+      // fprintf(stderr, "\n old symbol = %s %p\n", tort_symbol_data(e->value), (void *) e->value);
+      return e->value;
+    } else {
+      tort_v key, value;
+      key = tort_string_new_cstr(string);
+      value = tort_allocate(0, 0, sizeof(tort_symbol), _tort->_mt_symbol);
+      tort_ref(tort_symbol, value)->name = key;
+      _tort_m_map__add(tort_thread_arg _tort->symbols, key, value);
+      // fprintf(stderr, "\n new symbol = %s %p\n", tort_symbol_data(value), (void *) value);
+      return value;
+    } 
   } else {
     tort_v value;
     value = tort_allocate(0, 0, sizeof(tort_symbol), _tort->_mt_symbol);
