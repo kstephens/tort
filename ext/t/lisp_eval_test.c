3d57a6306547c820f3ad44fe73306e0164d19880
Kurt Stephens <ks.github@kurtstephens.com>
2010-12-29 06:39:27 -0600

    Lisp REPL prototype.

---
+++ ext/t/lisp_eval_test.c
@@ -0,0 +1,24 @@
+#include "tort/tort.h"
+#include "tort/block.h"
+
+#include <stdio.h>
+
+
+int main(int argc, char **argv, char **environ)
+{
+  tort_v in, out;
+  tort_v v;
+ 
+  tort_runtime_create();
+  tort_send(tort_s(_dlopen), tort_string_new_cstr(LIB_DIR "/libtortext.dylib"));
+
+  in = tort_stdin;
+  out = tort_stdout;
+
+  v = tort_send(tort_s(lisp_repl), in, out, tort_(root));
+
+  tort_printf(out, "\n\nDONE\n");
+
+  return 0;
+}
+
