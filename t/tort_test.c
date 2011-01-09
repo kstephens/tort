a8cb50f5d9380c5ca6fdbe668bc2173164a578f1
Kurt Stephens <ks.github@kurtstephens.com>
2011-01-09 09:44:57 -0600

    Cleanup.

--- t/tort_test.c
+++ t/tort_test.c
@@ -1,23 +1,14 @@
 #include "tort/tort.h"
-#include "tort/block.h"
-
 #include <stdio.h>
-
 
 int main(int argc, char **argv, char **environ)
 {
   tort_v io;
-
   tort_runtime_create();
-
   io = tort_stdout;
-
   tort_printf(io, "HELLO!\n");
-
   tort_send(tort_s(__debugger), io);
-
   printf("\nDONE\n");
-
   return 0;
 }
 
