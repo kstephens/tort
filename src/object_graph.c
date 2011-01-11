011c496b6e1d2d27f90d3a51c62f258c3cea6192
Kurt Stephens <ks.github@kurtstephens.com>
2011-01-10 22:16:33 -0600

    Preliminary object_graph improvements.

--- src/object_graph.c
+++ src/object_graph.c
@@ -6,6 +6,23 @@ typedef struct tort_og_context
 {
   FILE *fp;
   int edgeid;
+
+  struct slot {
+    const char *name;
+    tort_v value;
+    const char *style;
+  } *slots;
+  int slots_n;
+
+  struct link {
+    tort_v src_obj;
+    const char *src_slot;
+    tort_v dst_obj;
+    const char *dst_slot;
+    const char *style;
+  } *links;
+  int links_n;
+
   struct visited {
     void *ptr;
     struct visited *next;
@@ -34,15 +51,86 @@ static void visited(tort_og_context *context, void *ptr)
   context->visited = v;
 }
 
+static void link(tort_og_context *context, tort_v src_obj, const char *src_slot, tort_v dst_obj, const char *dst_slot, const char *style)
+{
+  struct link *link = &context->links[context->links_n ++];
+  link->src_obj = src_obj;
+  link->src_slot = src_slot;
+  link->dst_obj = dst_obj;
+  link->dst_slot = dst_slot;
+  link->style = style;
+}
+
+static const char *slot_str(tort_og_context *context, struct slot *slot, tort_v val, const char *port)
+{
+  tort_mtable *mt = tort_h_mtable(slot->value);
+  if ( ! port ) port = slot->name;
+  if ( 0 ) {
+  } else if ( val == tort_nil ) {
+    return "nil";
+  } else if ( val == 0 ) {
+    return "NULL";
+  } else if ( mt == tort__mt(tagged) ) {
+    return tort_sprintf(tort_nil, "%lld", (long long) tort_I(val));
+  } else if ( mt == tort__mt(string) ) {
+    return tort_sprintf(tort_nil, "\"%s\"", tort_string_charP(val));
+  } else if ( mt == tort__mt(symbol) ) {
+    return tort_symbol_charP(val);
+  } else {
+    return tort_object_name_(val);
+  }
+}
+
+static void slot_value(tort_og_context *context, tort_v obj, struct slot *slot)
+{
+  tort_v val = slot->value;
+  tort_mtable *mt = tort_h_mtable(slot->value);
+
+  fprintf(FP, "<TR %s>", slot->style ? slot->style : "");
+  if ( 0 ) {
+  } else if ( mt == tort__mt(pair) ) {
+    tort_pair *o = obj;
+    char *port_first = tort_sprintf(0, "%s_first", slot->name);
+    char *port_second = tort_sprintf(0, "%s_second", slot->name);
+    fprintf(FP, "<TD PORT=\"%s_first\">%s</TD><TD PORT=\"%s_second\">%s</TD>",
+	    port_first,
+	    slot_str(context, slot, o->first, "_first"),
+	    port_second,
+	    slot_str(context, slot, o->second, "_second"));
+    link(context, obj, port_first, o->first, 0, 0);
+    link(context, obj, port_second, o->second, 0, 0);
+  } else {
+    fprintf(FP, "<TD>%s</TD>", slot->name);
+    fprintf(FP, "<TD PORT=\"%s\">%s</TD>", slot->name, tort_object_name_(val));
+    link(context, obj, slot->name, val, 0, 0);
+  }
+  fprintf(FP, "</TR>");
+}
+
+
+static void slot(tort_og_context *context, tort_v obj, const char *slot_name, tort_v slot_val, const char *style)
+{
+  struct slot *slot = &context->slots[context->slots_n ++];
+  slot->name = slot_name;
+  slot->style = style;
+  slot->value = slot_val;
+
+  slot_value(context, obj, slot);
+}
+
+
 static void g1(tort_og_context *context, tort_v obj)
 {
   tort_mtable *mt = tort_h_mtable(obj);
   tort_mtable *cls_mt = tort_h_mtable(mt);
   const char *obj_name;
-  void *links[16][2];
+  void *links[16][3];
   int linksn = 0;
 
+  (void) slot;
+
 #define LINK_PTR(NAME, PTR)						\
+  links[linksn][2] = 0;							\
   if ( linksn >= 15 ) {							\
     links[linksn][0] = "...";						\
     fprintf(FP, " | ...");						\
@@ -55,6 +143,7 @@ static void g1(tort_og_context *context, tort_v obj)
   }									\
 
 #define LINK(NAME) LINK_PTR(#NAME, o->NAME)
+#define LINK_STYLE(STYLE) links[linksn - 1][2] = "style = \"" #STYLE "\""
 
   if ( obj == tort_nil ) {
     fprintf(FP, "node \"nil\";\n");
@@ -82,6 +171,7 @@ static void g1(tort_og_context *context, tort_v obj)
 	  (unsigned long long) tort_h(obj)->alloc_size);
 
   LINK_PTR("mtable", mt);
+  LINK_STYLE("dashed");
 
   fprintf(FP, " | <_> %p %s", 
 	  (void*) obj, obj_name ? obj_name : "");
@@ -151,11 +241,13 @@ static void g1(tort_og_context *context, tort_v obj)
       fprintf(FP, "\
 \"node%p\":%s -> \"node%p\":_ [ \n\
   id = %d  \n\
+  %s \n\
 ]; \n\
 ",
 	      obj, (const char*) links[i][0], 
 	      ptr,
-	      EDGEID ++);
+	      EDGEID ++,
+	      (char*) (links[i][2] ? links[i][2] : ""));
     
     }
   }
