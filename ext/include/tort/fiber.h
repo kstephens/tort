2a9eb98660b79473bac697ade6285d05c3b15aad
Kurt Stephens <ks.github@kurtstephens.com>
2011-04-23 01:09:49 -0500

    Use getcontext(), setcontext() for fibers.

--- ext/include/tort/fiber.h
+++ ext/include/tort/fiber.h
@@ -4,6 +4,16 @@
 #include "tort/core.h"
 
 #include <setjmp.h>
+#if defined(__APPLE__)
+/* OS X annoyance: http://duriansoftware.com/joe/PSA:-avoiding-the-%22ucontext-routines-are-deprecated%22-error-on-Mac-OS-X-Snow-Leopard.html */
+#include <sys/ucontext.h>
+int  getcontext(ucontext_t *);
+void makecontext(ucontext_t *, void (*)(), int, ...);
+int  setcontext(const ucontext_t *);
+int  swapcontext(ucontext_t * __restrict, const ucontext_t * __restrict);
+#else
+#include <ucontext.h>
+#endif
 
 #define TORT_FIBER_VOLATILE
 #ifndef TORT_FIBER_VOLATILE
@@ -29,9 +39,15 @@ enum tort_fiber_status_t {
 struct tort_fiber_t {
   void *data;
   tort_fiber_status_t status;
+#if 1
+  ucontext_t _ucontext;
+#define _tort_setjmp(F) getcontext(&(F)->_ucontext)
+#define _tort_longjmp(F, V) setcontext(&(F)->_ucontext)
+#else
   jmp_buf _jb;
 #define _tort_setjmp(F) setjmp((F)->_jb)
 #define _tort_longjmp(F, V) longjmp((F)->_jb, (V))
+#endif
   struct tort_fiber_t *parent;
   tort_fiber_func func;
   void *func_result;
