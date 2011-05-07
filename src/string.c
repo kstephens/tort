492231a0db263c89cb8dd69d2f2332c722f38dbc
Kurt Stephens <ks.github@kurtstephens.com>
2011-05-07 17:38:08 -0500

    Added string::escape!.

--- src/string.c
+++ src/string.c
@@ -28,6 +28,44 @@ tort_v _tort_M_string__new(tort_tp tort_mtable *mtable, tort_v size)
   return_tort_send(tort__s(_new), mtable, 0, tort_I(size));
 }
 
+tort_v _tort_m_string__escapeE(tort_tp tort_string *str)
+{
+  char *dst, *src = str->data, *src_end = str->data + str->size;
+  dst = src;
+  while ( src < src_end ) {
+    if ( *(src ++) == '\\' && src < src_end ) {
+      int c;
+      switch ( (c = *(src ++)) ) {
+      case 'a': c = '\a'; break;
+      case 'b': c = '\b'; break;
+      case 'e': c = '\e'; break;
+      case 'n': c = '\n'; break;
+      case 'r': c = '\r'; break;
+      case 't': c = '\t'; break;
+      case '0':
+	c -= '0'; 
+	c <<= 3; c += *(src ++) - '0';
+	c <<= 3; c += *(src ++) - '0';	
+      case 'x': {
+	{ int x = c;
+	  x = x >= 'a' ? x - 'a' + 10 : x >= 'A' ? x - 'A' + 10 : x - '0';
+	  c = x;
+	  x = *(src ++);
+	  x = x >= 'a' ? x - 'a' + 10 : x >= 'A' ? x - 'A' + 10 : x - '0';
+	  c <<= 4; c += x;
+	}
+      }
+      }
+      *(dst ++) = c;
+    } else {
+      dst ++;
+    }
+  }
+  *dst = 0;
+  str->size = dst - str->data;
+  return str;
+}
+
 /********************************************************************/
 
 tort_v tort_string_new(const char *ptr, size_t size)
