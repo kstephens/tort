fa663f1a539b1191e1b7f3b9552bfb3ad070b10e
Kurt Stephens <ks.github@kurtstephens.com>
2011-05-07 17:38:41 -0500

    Relational ops return booleans.

--- src/integer.c
+++ src/integer.c
@@ -9,20 +9,12 @@
 
 #define UOP(N,OP)						   \
   tort_v _tort_m_tagged__##N(tort_tp tort_v a)			   \
-  {								   \
-    return tort_i(OP tort_I(a));				   \
-  } 
-
+  { return tort_i(OP tort_I(a)); } 
 #define BOP(N,OP)						     \
   tort_v _tort_m_tagged__##N(tort_tp tort_v a, tort_v b)	     \
-  {								     \
-    return tort_i(tort_I(a) OP tort_I(b));			     \
-  } 
-
+  { return tort_i(tort_I(a) OP tort_I(b)); }
 #define ROP(N,OP)						    \
   tort_v _tort_m_tagged__##N(tort_tp tort_v a, tort_v b)	    \
-  {								    \
-    return tort_i(tort_I(a) OP tort_I(b));			    \
-  } 
-
+  { return (tort_I(a) OP tort_I(b)) ? tort_true : tort_false; } 
 #include "tort/ops.h"
+
