469cd92ad370d26c221556a9426c2896bd93a19d
Kurt Stephens <ks.github@kurtstephens.com>
2011-06-25 02:38:35 -0500

    Support for debuging boot.lisp.

--- lisp/bin/tlisp.c
+++ lisp/bin/tlisp.c
@@ -12,14 +12,20 @@ int main(int argc, char **argv, char **environ)
   out = tort_stdout;
 
   {
+    extern int _tort_lisp_trace;
     const char *str = getenv("TORT_LISP_LIB_DIR");
     tort_v boot = tort_string_new_cstr(str && *str ? str : TORT_LISP_LIB_DIR);
     tort_v io;
+    tort_v out_io;
+    str = getenv("TORT_LISP_BOOT_DEBUG");
+    out_io = str && *str ? tort_stderr : tort_nil;
+    if ( str && *str ) _tort_lisp_trace ++;
     tort_send(tort_s(append), boot, tort_string_new_cstr("/boot.lisp"));
     tort_printf(tort_stdout, ";; %s: reading %T\n", argv[0], boot);
     io = tort_send(tort_s(__create), tort__mt(io), (FILE*) 0);
     io = tort_send(tort_s(open), io, boot, tort_string_new_cstr("r"));
-    env = tort_send(tort_s(lisp_repl), io, tort_nil, tort_nil, tort_nil);
+    env = tort_send(tort_s(lisp_repl), io, out_io, tort_nil, tort_nil);
+    _tort_lisp_trace --;
   }
 
   env = tort_send(tort_s(lisp_repl), in, out, out, env);
