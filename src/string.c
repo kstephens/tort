4083ce9f6b037bc271bca6417bf547817dbe4afc
Kurt Stephens <ks.github@kurtstephens.com>
2011-06-25 02:42:06 -0500

    Support for C string unescapes.

--- src/string.c
+++ src/string.c
@@ -41,18 +41,23 @@ tort_v _tort_m_string__escapeE(tort_tp tort_string *str)
       case 'n': c = '\n'; break;
       case 'r': c = '\r'; break;
       case 't': c = '\t'; break;
-      case '0':
+      case '0' ... '7':
 	c -= '0'; 
+	if ( ! *src ) break;
 	c <<= 3; c += *(src ++) - '0';
+	if ( ! *src ) break;
 	c <<= 3; c += *(src ++) - '0';	
+	break;
       case 'x': {
 	{ int x = c;
 	  x = x >= 'a' ? x - 'a' + 10 : x >= 'A' ? x - 'A' + 10 : x - '0';
 	  c = x;
+	  if ( ! *src ) break;
 	  x = *(src ++);
 	  x = x >= 'a' ? x - 'a' + 10 : x >= 'A' ? x - 'A' + 10 : x - '0';
 	  c <<= 4; c += x;
 	}
+	break;
       }
       }
       *(dst ++) = c;
@@ -65,7 +70,36 @@ tort_v _tort_m_string__escapeE(tort_tp tort_string *str)
   return str;
 }
 
-/********************************************************************/
+tort_v _tort_m_string__unescape(tort_tp tort_string *str)
+{
+  tort_string *result = tort_send(tort__s(clone), str);
+  char *dst, *src = str->data, *src_end = str->data + str->size;
+  tort_send(tort__s(_resize), result, (size_t) str->size * 4); /* 4 = '\x00' or '\000' */
+  dst = result->data;
+  while ( src < src_end ) {
+    int c = *(src ++);
+    if ( 32 <= c || c < 127 ) { 
+      dst ++;
+    } else {
+      switch ( c ) {
+      case '\a': *(dst ++) = '\\'; c = 'a'; break;
+      case '\b': *(dst ++) = '\\'; c = 'b'; break;
+      case '\e': *(dst ++) = '\\'; c = 'e'; break;
+      case '\n': *(dst ++) = '\\'; c = 'n'; break;
+      case '\r': *(dst ++) = '\\'; c = 'r'; break;
+      case '\t': *(dst ++) = '\\'; c = 't'; break;
+      default:
+	*(dst ++) = '\\';
+	*(dst ++) = '0' + ((c >> 6) & 0x7);
+	*(dst ++) = '0' + ((c >> 3) & 0x7);
+	c         = '0' + ((c >> 0) & 0x7);
+      }
+      *(dst ++) = c;
+    }
+  }
+  *dst = 0;
+  return_tort_send(tort__s(_resize), result, dst - result->data);
+}
 
 tort_v tort_string_new(const char *ptr, size_t size)
 {
