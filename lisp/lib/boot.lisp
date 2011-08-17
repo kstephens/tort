19872d58ab99fa6e32ebdbca6997f5bcb1aecdde
Kurt Stephens <ks.github@kurtstephens.com>
2011-08-16 21:18:19 -0500

    More scheme support.

--- lisp/lib/boot.lisp
+++ lisp/lib/boot.lisp
@@ -3,9 +3,6 @@
 
 (define %root (lambda (sym) ('get &root sym)))
 (define %mtable-by-name (lambda (sym) ('get (%root 'mtable) sym)))
-(define *standard-input*  (%root 'stdin))
-(define *standard-output* (%root 'stdout))
-(define *standard-error*  (%root 'stderr))
 
 (define %get-type (lambda (o) ('_mtable o)))
 (define eq? (lambda (a b) ('eq? a b)))
@@ -24,9 +21,26 @@
 (define cdr (lambda (o) ('cdr o)))
 (define set-car! (lambda (o v) ('set-car! o v)))
 (define set-cdr! (lambda (o v) ('set-cdr! o v)))
-
 (define list (lambda args args))
+(define map 
+  (lambda (l f)
+    (if (null? l)
+	l
+      (cons (f (car l))
+	    (map (cdr l) f)))))
+(define for-each
+  (lambda (l f)
+    (if (null? l)
+	l
+      (begin
+       (f (car l))
+       (for-each (cdr l) f)))))
 
+
+(define <string> (%mtable-by-name 'string))
+(define string? 
+  (lambda (o) (eq? (%get-type o) <string>)))
+(define string-length (lambda (s) ('size s)))
 (define string-ref (lambda (s i) ('get s i)))
 (define string-set! (lambda (s i v) ('set s i v)))
 (define string-append 
@@ -34,10 +48,44 @@
     (set-car! args ('clone (car args)))
     (%reduce 'append args)))
 
+(define <symbol> (%mtable-by-name 'symbol))
+(define symbol?
+  (lambda (o) (eq? (%get-type o) <symbol>)))
+(define make-symbol 
+  (lambda (s) ('_create <symbol> s)))
+
+(define *standard-input*  (%root 'stdin))
+(define *standard-output* (%root 'stdout))
+(define *standard-error*  (%root 'stderr))
+(define <io> (%mtable-by-name 'io))
+(define open-output-file
+  (lambda (fname)
+    ('open ('create <io>) fname "w+")))
+(define close-output-file 
+  (lambda (f)
+    ('close f)))
+(define open-input-file
+  (lambda (fname)
+    ('open ('create <io>) fname "r")))
+(define close-input-file 
+  (lambda (f)
+    ('close f)))
+
+(define newline
+  (lambda port
+    ('_write (if (pair? port) (car port) *standard-output*) "\n")))
+
 (define write 
   (lambda (obj . port)
     ('lisp_write obj 
 		 (if (pair? port) (car port) *standard-output*))))
+
+(define display
+  (lambda (obj . port)
+    (set! port (if (pair? port) (car port) *standard-output*)) 
+    (if (string? obj)
+	('_write port obj)
+        (write obj port))))
 
 (define %reduce 
   (lambda (f l)
@@ -77,15 +125,27 @@
 
 (define = eq?)
 
-(define <io> (%mtable-by-name 'io))
-
-(define *load-debug* #t)
-(define load (lambda (fname . env))
-  ('lisp_repl ('open ('create <io>) fname "r") 
-	      (if *load-debug* *standard-error* nil)
-	      (if *load-debug* (string-append fname ": ") nil)
-	      (if (null? env) &env (car env))))
+(define *load-debug* #f)
+(define load 
+  (lambda (fname . env)
+    (let ((f (open-input-file fname))
+	  (out (if *load-debug* *standard-error* nil))
+	  (env (if (null? env) &env (car env))))
+      ; (display "opened ")(write fname)(display " => ")(write f)(newline)
+      (let ((r ('lisp_repl f out out env)))
+	(close-input-file f)
+	(r)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define <posix> (%mtable-by-name 'posix))
+(define posix ('allocate <posix>))
+(define posix:system (lambda (str) ('system posix str)))
+
+(posix:system "hostname")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(set! *load-debug* #t)
+(load "compiler/lisp/compiler.scm")
 
