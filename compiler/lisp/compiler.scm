4b5ce8d704051f414ab5f2e69be763114dc821e9
Kurt Stephens <kurt@enovafinancial.com>
2011-10-28 19:24:33 -0500

    Support for literal object expressions.

--- compiler/lisp/compiler.scm
+++ compiler/lisp/compiler.scm
@@ -3,6 +3,7 @@
     (list 
      ""   ; output stream
      nil  ; output name
+     0    ; next label id
      )))
 
 (define compiler:output-name 
@@ -16,6 +17,21 @@
 (define compiler:set-stream!
   (lambda (c s)
     (set-car! c s)))
+
+(define compiler:label-id
+  (lambda (c)
+    (caddr c)))
+
+(define compiler:set-label-id!
+  (lambda (c v)
+    (set-car! (cddr c) v)))
+
+(define compiler:label
+  (lambda (c)
+    (let* ((id (compiler:label-id c))
+	   (label (string-concat "L" (number->string id))))
+      (compiler:set-label-id! c (+ id 1))
+      label)))
 
 (define compiler:emit 
   (lambda (c . args)
@@ -49,10 +65,48 @@
 
 (define compiler:method:body 
   (lambda (c o)
-    (compiler:emit c "movq $42, %rax")
-    (compiler:emit c "addq %rax, %rax")
-    (compiler:emit c "orq  $1, %rax")
+    (compiler:expr c o)
+    ;; (compiler:expr c 42)
     ))
+
+(define compiler:expr
+  (lambda (c o)
+    (if (tagged? o)
+	(compiler:literal:tagged c o)
+        (compiler:literal:constant c o))
+    ))
+
+(define compiler:literal:tagged
+  (lambda (c o)
+    (compiler:emit c "movq $" (| (+ o o) 1) ", %rax" 
+                     " // literal " (object->string o))
+    ))
+
+(define compiler:literal:constant
+  (lambda (c o)
+    (let ((ptr (send '_object_ptr o)))
+      (compiler:emit c "movq $0x" (send '_to_string ptr) ", %rax"
+                     " // constant object ref " ptr))
+    ))
+
+(define compiler:literal:string
+  (lambda (c o)
+    (let ((v   (compiler:label c))
+	  (s   (compiler:label c)))
+      (compiler:emit c ".data")
+      (compiler:emit c v ":")
+      (compiler:emit c ".asciiz" ('escape o))
+      (compiler:emit c s ":")
+      (compiler:emit c ".qword $0")
+      (compiler:emit c ".text")
+      )))
+
+(define compiler:expr:from-word
+  (lambda (c o)
+    ; int expression is in %rax,
+    ; (compiler:emit c "movq $42, %rax")
+    (compiler:emit c "addq %rax, %rax")
+    (compiler:emit c "orq  $1, %rax")))
 
 (define compiler:assemble
   (lambda (c . options)
