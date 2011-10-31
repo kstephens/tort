5c783aa8a054a0cecff013b62a05dd05653e853d
Kurt Stephens <ks.github@kurtstephens.com>
2011-10-31 04:24:12 -0500

    Additional box/unbox methods.

--- core/include/tort/box.h
+++ core/include/tort/box.h
@@ -10,4 +10,13 @@
 #define tort_box_size_t(V)tort_i(V)
 #define tort_unbox_size_t(V)tort_I(V)
 
+#define tort_box_int(V)tort_i(V)
+#define tort_unbox_int(V)tort_I(V)
+
+#define tort_box_charP(V)tort_string_new_cstr(V)
+#define tort_unbox_charP(V)tort_string_charP(V)
+
+#define tort_box_voidP(V)tort_ptr_new(V)
+#define tort_unbox_voidP(V)tort_ptr_data(V)
+
 #endif
