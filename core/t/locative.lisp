4cace0e4ca4e031812f3ae8f946f45a790fc5f4b
Kurt Stephens <ks.github@kurtstephens.com>
2011-11-16 01:44:42 -0600

    Improved locative tests; using locative as method still fails.

--- core/t/locative.lisp
+++ core/t/locative.lisp
@@ -1,13 +1,19 @@
-(define c (cons 0 1))
-c
-(define l0 ('_slot_locative_at c 0))
-l0
-('value l0)
-('value= l0 'a)
-c
-(define o ('new <object>))
-o
-('_set_mtable o ('new <mtable> ('_mtable o)))
-('add_method ('_mtable o) 'foo l0)
-('foo o)
-
+(let ((c (cons 0 1))
+      (l #f)
+      (o #f)
+      (sym (make-symbol '())))
+  (write c)(newline)
+  (set! l ('_slot_locative_at c 0))
+  (write l)(newline)
+  ('value l)
+  ('value= l 'changed-by-locative-value=)
+  (write c)(newline)
+  (set! o (cons 2 3))
+  ;; Give object its own mtable.
+  ; ('_mtable= o ('new <mtable> ('_mtable o)))
+  (write sym)(newline)
+  ('add_method ('_mtable o) sym l)
+  (set! &trace 2)
+  (write (sym o))(newline)
+  (set! &trace 0)
+  )
