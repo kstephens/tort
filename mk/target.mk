49104c738f61fce2fb3c554fc345f66088db336b
Kurt Stephens <ks.github@kurtstephens.com>
2011-11-26 12:16:35 -0600

    Remove unnecessary whitespace.

--- mk/target.mk
+++ mk/target.mk
@@ -31,35 +31,23 @@ TEST_FILES = $(TEST_C_FILES:.c=)
 TEST_OUT_FILES = $(TEST_C_FILES:.c=.out)
 
 ######################################################################
-# default:
-#
 
 all : components tests
 	@for d in $(SUBDIRS) .; do [ $$d = '.' ] && break; $(MAKE) -wC "$$d" all; done
-
 components : early $(GEN_H_FILES) $(GEN_C_FILES) libs bins
-
 libs : $(LIBS_EARLY) $(GEN_LIBS)
-
 bins : $(BINS_EARLY) $(GEN_BINS)
-
 $(BINS_EARLY) $(GEN_BINS) : $(LINK_DEPS)
-
 early : early-headers early-files check-gen-new
-
 early-headers: boot/include/.touch
-
 GEN_FILES_NEW=$(GEN_FILES:%.gen=%.new)#
-
 check-gen-new : $(GEN_FILES_NEW)
 	@set -e ;\
 	for f in $(GEN_FILES_NEW:%.new=%) ;\
 	do \
 	  cmp -s "$$f.new" "$$f" || cp -p "$$f.new" "$$f" ;\
 	done
-
 boot: boot/include/.touch
-
 boot/include/.touch :
 	@mkdir -p $$(dirname $@)
 	set -ex; for f in $(GEN_H_FILES); do \
@@ -71,25 +59,17 @@ boot/include/.touch :
 	touch $@
 
 ######################################################################
-# include:
-#
 
 includes : $(GEN_H_FILES)
-
 early-files: $(GEN_H_FILES:%=%.new)
-
 %.new : %.gen
 	$(@:.new=).gen $(@:.new=) $@
 
 ######################################################################
-# src:
-#
 
 srcs : $(GEN_C_FILES)
 
 ######################################################################
-# object:
-#
 
 %.lo : %.c
 	$(COMPILE.c) $(CFLAGS_DEBUG) -o $@ $<
@@ -101,71 +81,53 @@ srcs : $(GEN_C_FILES)
 	$(CC_BASE:%-g%=%%) -S -o $@ $<
 
 ######################################################################
-# executable:
-#
 
 % : %.lo
 	$(LIBTOOL) --tag=LD --mode=link $(CC) $(CFLAGS) $(CFLAGS_DEBUG) $(LDFLAGS) $@.lo $(BIN_LIBS) $(LIBS) -o $@
 
 
 ######################################################################
-# library:
-#
 
 $(LIB) : $(LIB_OFILES) $(LINK_DEPS)
 	$(LIBTOOL) --tag=LD --mode=link $(CC) $(LDFLAGS) $(LIB_FLAGS) -o $@ $(LIB_OFILES) $(LIBS)
 	$(LIBTOOL) --mode=install cp $@ $(libdir)
-
 $(LIB_OFILES) : $(LIB_HFILES)
 
 ######################################################################
-# testing:
-#
 
 # TEST_LIBS = $(LIB_TORT)
 
 %.t : %.c 
 	$(LIBTOOL) --tag=LD --mode=link $(CC) $(CFLAGS) $(LDFLAGS) $(@:.t=.c) $(TEST_LIBS) $(LIBS) -o $@
-
 tests : components $(TEST_T_FILES) 
-
 $(TEST_T_FILES) : $(TEST_LIBS) $(LINK_DEPS)
-
 $(TEST_T_FILES) $(LIB_OFILES) : $(GEN_H_FILES) include/tort/*.h 
-
 run-test : tests
 	@set -ex; for f in $(TEST_T_FILES); do \
 	  $$f ;\
 	done
 	@for d in $(SUBDIRS) .; do [ $$d = '.' ] && break; $(MAKE) -wC "$$d" run-test; done
-
 test : tests
 	@echo "Testing:"
 	$(BASE_DIR)/tool/t run $(TEST_FILES)
 	@for d in $(SUBDIRS) .; do [ $$d = '.' ] && break; $(MAKE) -wC "$$d" test; done
-
 valgrind : tests
 	@echo "Valgrind:"
 	$(BASE_DIR)/tool/t valgrind $(TEST_FILES)
 	@for d in $(SUBDIRS) .; do [ $$d = '.' ] && break; $(MAKE) -wC "$$d" valgrind; done
-
 accept-test : tests
 	$(BASE_DIR)/tool/t accept $(TEST_FILES)
-
 accept-all-test : tests
 	$(BASE_DIR)/tool/t accept $(TEST_FILES)
 	@for d in $(SUBDIRS) .; do [ $$d = '.' ] && break; $(MAKE) -wC "$$d" accept-all-test; done
 
 ######################################################################
-# maint:
-#
 
 clean ::
 	rm -f $(TEST_T_FILES) $(GEN_LIBS) $(GEN_BINS) src/*{.o,.lo,.la} t/*.{t,out} $(GEN_C_FILES) $(GEN_H_FILES) .stats/*
 	rm -rf boot/include
 	find . -name '*.dSYM' -type d -print0 | xargs -0 rm -rf
 	@for d in $(SUBDIRS) .; do [ "$$d" = '.' ] && break; $(MAKE) -wC "$$d" clean; done
-
 veryclean :: very-clean
 very-clean :: clean
 
