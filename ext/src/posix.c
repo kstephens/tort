145820b5a976cfc0b1d49597dcc4eb96a7caf945
Kurt Stephens <ks.github@kurtstephens.com>
2011-12-03 12:17:51 -0600

    Additional posix interfaces.

--- ext/src/posix.c
+++ ext/src/posix.c
@@ -1,7 +1,22 @@
 #include "tort/tort.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h> /* sys_nerr, sys_errlist */
+#include <errno.h> /* errno */
 
+tort_v _tort_m_posix__errno(tort_tp tort_v rcvr)
+{
+  return tort_i(errno);
+}
+tort_v _tort_m_posix__errnoSET(tort_tp tort_v rcvr, tort_v v)
+{
+  return tort_i(errno == tort_I(v));
+}
+tort_v _tort_m_posix__errstr(tort_tp tort_v rcvr, tort_v v)
+{
+  int i = tort_I(v);
+  return 0 <= i && i < sys_nerr ? tort_string_new_cstr(sys_errlist[i]) : tort_false;
+}
 tort_v _tort_m_posix__getpid(tort_tp tort_v rcvr)
 {
   return tort_i(getpid());
@@ -36,6 +51,11 @@ tort_v _tort_m_posix__exit(tort_tp tort_v rcvr, tort_v code)
 tort_v _tort_m_posix__system(tort_tp tort_v rcvr, tort_string *str)
 {
   return tort_i(system(tort_string_data(str)));
+}
+
+tort_v _tort_m_posix__unlink(tort_tp tort_v rcvr, tort_string *str)
+{
+  return tort_i(unlink(tort_string_data(str)));
 }
 
 tort_v tort_runtime_initialize_posix()
