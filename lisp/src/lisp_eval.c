31915e3d7053ddbf31dbbf3b4f48fdad03eea73d
Kurt Stephens <ks.github@kurtstephens.com>
2012-02-14 18:09:29 -0600

    Refactor lisp_repl into lisp_repl.c.

--- lisp/src/lisp_eval.c
+++ lisp/src/lisp_eval.c
@@ -534,60 +534,6 @@ tort_v _tort_m_object__lisp_eval_body(tort_tp tort_cons *obj, tort_v env)
   return_tort_send(tort_s(lisp_eval), obj->car, env);
 }
 
-//////////////////////////////////////////////////////////////////////
-
-tort_v _tort_m_lisp_repl__read(tort_tp tort_repl *repl)
-{
-  repl->expr = tort_send(tort_s(lisp_read), repl->input);
-  return repl;
-}
-tort_v _tort_m_lisp_repl__new_environment(tort_tp tort_repl *repl)
-{
-  tort_lisp_environment *env = 
-    tort_send(tort__s(new), tort_mt(lisp_environment), 
-	      tort_nil, tort_nil, tort_nil, tort_nil);
-  tort_send(tort_s(add), env, tort_s(ANDrepl), repl);
-  repl->env = env;
-  return repl;
-}
-tort_v _tort_m_lisp_repl__caughtE(tort_tp tort_repl *repl, tort_v value)
-{
-  _tort_lisp_trace = _tort_lisp_macro_trace = 0;
-  return repl;
-}
-tort_v _tort_m_lisp_repl__eval(tort_tp tort_repl *repl)
-{
-  ((tort_lisp_environment*) repl->env)->msg = _tort_message->previous_message; // FIXME?
-  repl->result = tort_send(tort_s(lisp_eval_top_level), repl->expr, repl->env);
-  return repl;
-}
-tort_v _tort_m_lisp_repl__print(tort_tp tort_repl *repl, tort_v thing)
-{
-  tort_send(tort_s(lisp_write), thing, repl->output);
-  return repl;
-}
-
-tort_v _tort_m_lisp_repl__load(tort_tp tort_repl *repl_main, tort_v name)
-{
-  tort_repl *repl;
-  tort_v io, result;
-  repl = tort_send(tort_s(clone), repl_main);
-  repl->prompt_id = name;
-  if ( repl->main == tort_nil )
-    repl->main = repl_main;
-  io = tort_send(tort_s(new), tort__mt(io));
-  io = tort_send(tort_s(open), io, name, tort_string_new_cstr("r"));
-  if ( io == tort_nil )
-    return tort_error(tort_ta "load: cannot open %T\n", name);
-  if ( repl->message != tort_nil )
-    tort_printf(repl->message, ";; load %T\n", name);
-  repl->input = io;
-  result = tort_send(tort_s(run), repl);
-  if ( repl->message != tort_nil )
-    tort_printf(repl->message, ";; load %T: done\n", name);
-  return result;
-}
-
 tort_v tort_runtime_initialize_lisp_eval()
 {
   tort_send(tort_s(load), tort_mt(dynlib), tort_string_new_cstr("libtortext"));
