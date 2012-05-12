7d1dbed54f19484195e54e1d21972f0bf58f129c
Kurt Stephens <ks.github@kurtstephens.com>
2012-05-12 00:30:41 -0500

    Distingush between 32-bit and 64-bit test output.

--- lisp/t/lisp_eval_test.c
+++ lisp/t/lisp_eval_test.c
@@ -1,4 +1,5 @@
 #include "tort/tort.h"
+#include "tort/repl.h"
 
 #include <stdio.h>
 
@@ -6,6 +7,7 @@ int main(int argc, char **argv, char **environ)
 {
   tort_v in, out;
   tort_v v;
+  tort_repl *repl = tort_nil;
  
   tort_runtime_create();
   tort_send(tort_s(load), tort_mt(dynlib), tort_string_new_cstr("libtortlisp"));
@@ -13,7 +15,14 @@ int main(int argc, char **argv, char **environ)
   in = tort_stdin;
   out = tort_stdout;
 
-  v = tort_send(tort_s(lisp_repl), in, out, out, tort_nil);
+  repl = tort_send(tort_s(new), tort_mt(lisp_repl));
+  tort_send(tort_s(new_environment), repl);
+  repl->input = in;
+  repl->output = out;
+  repl->message = tort_nil;
+  repl->prompt = tort_nil;
+
+  v = tort_send(tort_s(run), repl);
 
   tort_printf(out, "\n\nDONE\n");
 
